9bf340df9492001f8825f7b1d17a791c71cf844a
chanmol1999 <chanmol1999@gmail.com>
2019-01-12 23:51:38 +0530

    Updated todo activity

--- app/src/main/java/com/example/shivansh/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/shivansh/to_dolist/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.shivansh.to_dolist;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
