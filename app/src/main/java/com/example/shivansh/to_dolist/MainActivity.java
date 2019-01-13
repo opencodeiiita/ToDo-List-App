0af4ea3205b9d9ddc2988220431dc6dc7b3c0bf6
Akshay Kalola <akshay.kalola28@gmail.com>
2019-01-13 13:44:13 +0530

    Issue completed.

--- app/src/main/java/com/example/shivansh/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/shivansh/to_dolist/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.shivansh.to_dolist;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,15 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
