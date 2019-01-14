c8e29c26181d8a0cbb89eacf5d29a0f32bab7731
Ritik Harchani <harchani.ritik@gmail.com>
2019-01-14 22:24:35 +0530

    Fixes Issue:#25 Rename class "todo" to NewToDoActivity

--- app/src/main/java/com/example/shivansh/to_dolist/NewToDoActivity.java
+++ app/src/main/java/com/example/shivansh/to_dolist/NewToDoActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class todo extends AppCompatActivity {
+public class NewToDoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
