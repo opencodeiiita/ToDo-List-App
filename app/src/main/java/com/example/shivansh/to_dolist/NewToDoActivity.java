acfab355969579b23a7f1c32d92c3d3a036d028e
Bindu Verma <37896216+BinduVerma123@users.noreply.github.com>
2019-01-25 14:08:09 -0800

    adding date picker

--- app/src/main/java/com/example/shivansh/to_dolist/NewToDoActivity.java
+++ app/src/main/java/com/example/shivansh/to_dolist/NewToDoActivity.java
@@ -4,13 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class NewToDoActivity extends AppCompatActivity {
+    EditText date;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newtodoactivity);
+         date = (EditText) findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(NewToDoActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
