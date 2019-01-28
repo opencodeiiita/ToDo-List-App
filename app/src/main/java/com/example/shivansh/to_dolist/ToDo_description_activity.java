981fabb0ce6db5e551511e0a8b159b54d2584393
Achint Agrawal <42510489+achintagrawal@users.noreply.github.com>
2019-01-29 01:34:59 +0530

    completed #12 task

---
+++ app/src/main/java/com/example/shivansh/to_dolist/ToDo_description_activity.java
@@ -0,0 +1,13 @@
+package com.example.shivansh.to_dolist;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class ToDo_description_activity extends MainActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_description_activity);
+ }
+}
