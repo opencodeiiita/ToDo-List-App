c8f82329b903a7ca35fc44e4a596c27897abc211
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-02-01 20:11:27 +0530

    Update DatabaseHelper.java

--- app/src/main/java/com/example/shivansh/to_dolist/DatabaseHelper.java
+++ app/src/main/java/com/example/shivansh/to_dolist/DatabaseHelper.java
@@ -29,4 +29,146 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
+    
+     boolean addEmployee(String id, String task, String description, String salary) {
+        ContentValues contentValues = new ContentValues();
+         boolean addEmployee(String id, String task, String description, String deadline) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, task);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, salary);
+        SQLiteDatabase db = getWritableDatabase();
+        return db.insert(TABLE_NAME, null, contentValues) != -1;
+    }
+ 
+ 
+    /*
+    * READ OPERATION
+    * =================
+    * Here we are reading values from the database
+    * First we called the getReadableDatabase() method it will return us the SQLiteDatabase instance
+    * but using it we can only perform the read operations.
+    *
+    * We are running rawQuery() method by passing the select query.
+    * rawQuery takes two parameters
+    * 1. The query
+    * 2. String[] -> Arguments that is to be binded -> We use it when we have a where clause in our query to bind the where value
+    *
+    * rawQuery returns a Cursor object having all the data fetched from database
+    * */
+    Cursor getAllEmployees() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+ 
+    /*
+    * UPDATE OPERATION
+    * ==================
+    */
+    boolean updateEmployee(int id, String name, String description, String deadline) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+      
+        contentValues.put(COL_2, task);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, deadline);
+        return db.update(TABLE_NAME, contentValues, COL_1 + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
+ 
+ 
+    /*
+    * DELETE OPERATION
+    * ======================
+    */
+     boolean addEmployee(String name, String dept, String joiningdate, double salary) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, task);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, deadline);
+        SQLiteDatabase db = getWritableDatabase();
+        return db.insert(TABLE_NAME, null, contentValues) != -1;
+    }
+ 
+ 
+    /*
+    * READ OPERATION
+    * =================
+    */
+    Cursor getAllEmployees() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+ 
+    /*
+    * UPDATE OPERATION
+    * ==================
+     */
+    boolean updateEmployee(int id, String name, String dept, double salary) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, task);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, salary);
+        return db.update(TABLE_NAME, contentValues, COLUMN_ID + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
+ 
+ 
+    /*
+    * DELETE OPERATION
+    * ======================
+   */
+    boolean deleteEmployee(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(TABLE_NAME, COL_1 + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
+    * 2. String -> The where clause passed as columnname = ? 
+    * 3. String[] -> The values to be binded on the where clause 
+    * */
+    boolean deleteEmployee(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(TABLE_NAME, COL_1 + "=?", new String[]{String.valueOf(id)}) == 1;
+   
+        contentValues.put(COLUMN_3, description);
+        contentValues.put(COLUMN_4, deadline);
+       
+        SQLiteDatabase db = getWritableDatabase();
+        return db.insert(TABLE_NAME, null, contentValues) != -1;
+    }
+ 
+ 
+    /*
+    * READ OPERATION
+    * =================
+    */
+    Cursor getAllEmployees() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+ 
+    /*
+    * UPDATE OPERATION
+    * ==================
+    */
+    boolean updateEmployee(int id, String name, String dept, double salary) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+       
+        contentValues.put(COL_2, task);
+        contentValues.put(COL_3, description);
+        contentValues.put(COL_4, salary);
+        return db.update(TABLE_NAME, contentValues, COL_1 + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
+ 
+ 
+    /*
+    * DELETE OPERATION
+    * ======================
+    */
+    boolean deleteEmployee(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(TABLE_NAME, COL_1 + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
 }
