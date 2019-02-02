67d0d708c4cbf32e6b88b336a51a154b8ae9b2b3
Ritik Harchani <harchani.ritik@gmail.com>
2019-02-02 13:41:49 +0530

    Solved #48 Add a new Splash Screen Activity

---
+++ app/src/main/java/com/example/shivansh/to_dolist/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.shivansh.to_dolist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        myThread.start();
+    }
+}
